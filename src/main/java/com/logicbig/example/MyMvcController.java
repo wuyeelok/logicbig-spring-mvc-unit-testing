0c4fc2db24df994ba6fc41e2ce19024b65252779
wuyeelok <wuyeelok@gmail.com>
2019-06-06 12:20:24 +0800

    Make the controller realistic

--- src/main/java/com/logicbig/example/MyMvcController.java
+++ src/main/java/com/logicbig/example/MyMvcController.java
@@ -2,14 +2,20 @@ package com.logicbig.example;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import hk.gov.ogcio.esip.service.ConfigUtilService;
+
 @Controller
 public class MyMvcController {
+
+	@Autowired
+	ConfigUtilService configUtilService;
 
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String prepareView(Model model) {
@@ -19,8 +25,8 @@ public class MyMvcController {
 
 	@RequestMapping(value = "/esip/home", method = RequestMethod.GET)
 	public String showCoverHomePage(ModelMap model, HttpServletRequest request) {
-		model.addAttribute("springDispatcherURLPattern", "");
-		model.addAttribute("junctionPath", "");
+		model.addAttribute("springDispatcherURLPattern", configUtilService.getSpringDispatcherURLPattern());
+		model.addAttribute("junctionPath", configUtilService.getJunctionPath());
 
 		return "esip/cover_home_page";
 	}
